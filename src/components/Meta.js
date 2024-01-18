4ab68bdd241323495727c0139085ec55e41c3bb8
veronica2235 <117762433+veronica2235@users.noreply.github.com>
2024-01-18 15:29:36 +0000

    mensaje

---
+++ src/components/Meta.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Meta(text){
+    return (
+        <div className="info-meta">
+            <div>
+            <p className="contenido">{text}</p>
+            </div>
+        <button onClick={""}><img src="" alt=""/></button>
+        <button onClick={""}><img src="" alt=""/></button>
+        </div>
+    );
+}
+export default Meta;
